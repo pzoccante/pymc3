ba6af6b1ab3fe2364c3ab8a23a9c6994f9dcfc46
pzoccante <paolo.zoccante@farfetch.com>
2018-04-03 16:55:53 +0000

    fix

--- pymc3/sampling.py
+++ pymc3/sampling.py
@@ -1336,8 +1336,9 @@ def init_nuts(init='auto', chains=1, n_init=500000, model=None,
         mu_approx = approx.approx.params[0]
         rho_approx = approx.approx.params[1]
 
-        print(mu_approx.eval())
-        print(rho_approx.eval())
+        _log.info('Starting values:')
+        _log.info(mu_approx.eval())
+        _log.info(rho_approx.eval())
 
         if numpy.isnan(mu_approx.eval()).any():
             mu_approx.set_value(np.zeros(mu_approx.eval().shape))
